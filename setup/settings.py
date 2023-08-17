d55d647b9e23a7291539230c5cee59a82fa494b9
Julio Gazoli <julio_gazoli@hotmail.com>
2023-08-17 14:31:44 -0300

    DRF e Swagger
    Swagger

--- setup/settings.py
+++ setup/settings.py
@@ -25,8 +25,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'django_filters',
+    'drf_yasg',
+
     'aluraflix',
 ]
 
